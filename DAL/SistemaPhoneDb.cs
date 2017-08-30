d9f9a67ef97f600924c284d140e9e7d3cd6e5b4f
Jose Ariel Paredes Gonzalez <jose_paredes2@ucne.edu.do>
2017-08-30 23:20:22 +0100

    DAL (Terminada)

---
+++ DAL/SistemaPhoneDb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class SistemaPhoneDb : DbContext
+    {
+        public SistemaPhoneDb() : base("ConStr")
+        {
+
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+    }
+}
---
+++ Entidades/Usuarios.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuarios
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Nombres { get; set; }
+        public string FechaCreacion { get; set; }
+        public string Contrasena { get; set; }
+    }
+}
